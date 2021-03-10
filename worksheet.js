3ef984cf012cc6aee9701128ab425192f49e4411
Steven <plumtree87@protonmail.com>
2021-03-10 05:25:07 +0500

    WOW THIS IS UGLY! I AM NOT AN ARTIST...

---
+++ worksheet.js
@@ -0,0 +1,15 @@
+let defintion = 4;
+
+function developYourGoal(defintionNumber){
+    let needSomethingDeveloped = true;
+    while (needSomethingDeveloped = true)
+    if (definitionNumber == 4 && hire == "y"){
+       needSomethingDeveloped = false;
+       console.log("got you covered")
+    }
+}
+
+let hire = prompt("Congrats on filling out the contact information\n"+
+  "press y to confirm.");
+
+developYourGoal(definition);
